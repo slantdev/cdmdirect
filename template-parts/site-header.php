8653f652ab151c6f27708d487878900d971d106b
Fransnico Susanto <fransnico@amazeworx.com>
2023-03-28 02:59:40 +0700

    fix navigation link

--- template-parts/site-header.php
+++ template-parts/site-header.php
@@ -20,12 +20,12 @@
       <div class="flex justify-end items-center gap-x-6">
         <div class="main-nav--div flex gap-x-10 items-center shadow-[0_1px_0px_rgba(255,255,255,0.5)]">
           <ul class="main-nav--ul flex gap-x-12 text-white font-medium uppercase">
-            <li><a href="/" class="inline-block whitespace-nowrap py-8 px-3 border-b-4 border-transparent hover:border-brand-orange transition duration-300">Home</a></li>
-            <li><a href="/our-services" class="inline-block whitespace-nowrap py-8 px-3 border-b-4 border-transparent hover:border-brand-orange transition duration-300">Services</a></li>
-            <li><a href="/industry" class="inline-block whitespace-nowrap py-8 px-3 border-b-4 border-transparent hover:border-brand-orange transition duration-300">Industry</a></li>
-            <li><a href="/resources" class="inline-block whitespace-nowrap py-8 px-3 border-b-4 border-transparent hover:border-brand-orange transition duration-300">Resources</a></li>
-            <li><a href="/about-us" class="inline-block whitespace-nowrap py-8 px-3 border-b-4 border-transparent hover:border-brand-orange transition duration-300">About</a></li>
-            <li><a href="/contact-us" class="inline-block whitespace-nowrap py-8 px-3 border-b-4 border-transparent hover:border-brand-orange transition duration-300">Contact Us</a></li>
+            <li><a href="<?php echo get_site_url() ?>/" class="inline-block whitespace-nowrap py-8 px-3 border-b-4 border-transparent hover:border-brand-orange transition duration-300">Home</a></li>
+            <li><a href="<?php echo get_site_url() ?>/our-services" class="inline-block whitespace-nowrap py-8 px-3 border-b-4 border-transparent hover:border-brand-orange transition duration-300">Services</a></li>
+            <li><a href="<?php echo get_site_url() ?>/industry" class="inline-block whitespace-nowrap py-8 px-3 border-b-4 border-transparent hover:border-brand-orange transition duration-300">Industry</a></li>
+            <li><a href="<?php echo get_site_url() ?>/resources" class="inline-block whitespace-nowrap py-8 px-3 border-b-4 border-transparent hover:border-brand-orange transition duration-300">Resources</a></li>
+            <li><a href="<?php echo get_site_url() ?>/about-us" class="inline-block whitespace-nowrap py-8 px-3 border-b-4 border-transparent hover:border-brand-orange transition duration-300">About</a></li>
+            <li><a href="<?php echo get_site_url() ?>/contact-us" class="inline-block whitespace-nowrap py-8 px-3 border-b-4 border-transparent hover:border-brand-orange transition duration-300">Contact Us</a></li>
           </ul>
           <a href="#" class="flex px-6 py-3 gap-x-4 text-white font-medium border border-solid border-white rounded-full bg-transparent hover:bg-brand-orange hover:border-brand-orange transition duration-200">
             <?php echo cdmdirect_icon(array('icon' => 'phone', 'group' => 'utilities', 'size' => '24', 'class' => 'text-white')); ?>
